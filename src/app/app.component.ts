7b877c1797b0d5738dc851d6de0d3a899647f153
jack <jackpipman@gmail.com>
2018-11-23 01:11:35 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'jp-shopmap';
+}
